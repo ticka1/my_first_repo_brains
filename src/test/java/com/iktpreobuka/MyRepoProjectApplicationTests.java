3a3868bfd74b0fa3ccea15791a2c8f27f86bae1c
Tijana Lucic <tijana.lucic.brains24@gmail.com>
2024-07-11 13:24:32 +0200

    initial comit

---
+++ src/test/java/com/iktpreobuka/MyRepoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRepoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
